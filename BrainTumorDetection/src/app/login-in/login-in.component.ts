fb3ce6527929f87fdd2a01958ade19b84f912d68
SanketMhaske1 <sanketmhaske005@gmail.com>
2024-04-25 17:24:39 +0530

    brain tumor ui upadte

---
+++ BrainTumorDetection/src/app/login-in/login-in.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './login-in.component.html',
+  styleUrl: './login-in.component.css'
+})
+export class LoginInComponent {
+
+}
